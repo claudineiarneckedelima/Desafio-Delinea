3af75e389cb48bcb0376b6431bb5e5e8c0b015c3
unknown <claudinei-de-lima@hotmail.com>
2019-03-01 15:25:01 -0300

    Update global.ts

--- global.ts
+++ global.ts
@@ -1,5 +1,7 @@
 export const GLOBAL = {
   nameApp: "CandidateApi",
-  urls:  "http://delineaapi.herokuapp.com/",
-  url: "https://delineaapi.herokuapp.com/"
+  url: "https://delineaapi.herokuapp.com/",
+  client_id: 'client_id=Rb6yDNb6muY6Wr9iGybl193VzO6BqOuleLGblg14',
+  client_secret :'client_secret=NjsLaIedGub9LC2xAKHIt7kiN4DiSBLolT74w2PYrOu4PPdRxCNqgZDLS1UlqwSQry2HSmRj21MWcOiKOuLq8UtsD0LBic26SxJAEHqf7AaZ5C6sOSG9WrHf3gOzJkmY',
+  grant_type:'grant_type=password'
 }

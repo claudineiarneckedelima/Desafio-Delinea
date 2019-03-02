660c8b5f7d92e3a73f213f88eb896dd7bc29bf53
unknown <claudinei-de-lima@hotmail.com>
2019-03-02 16:17:51 -0300

    Update dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GLOBAL } from '../../../../global';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  private candidates: any = [];
+  cTotal: any;
+  cPross: any;
+  cNPross: any;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.http.get(GLOBAL.url+'candidate').subscribe((res)=>{
+      for(let i in res){
+        this.candidates.push({
+          is_active: res[i].is_active
+        });
+      }
+    },
+    error => {
+        alert('Erro ao carregar lista de candidatos');
+    });
+  }
+
+  getCandidatesTotal(){
+    return this.candidates.length;
+  }
+
+  getCandidatesNoActive(){
+    let count = 0;
+    for(let i in this.candidates){
+      if(this.candidates[i].is_active === false)
+      count++;
+    }
+    return count;
+  }
+
+  getCandidatesActive(){
+    let count = 0;
+    for(let i in this.candidates){
+      if(this.candidates[i].is_active === true)
+      count++;
+    }
+    return count;
   }
 
 }

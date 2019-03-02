ca3b4f8330f2f389563e47acdf8385486a123c9a
unknown <claudinei-de-lima@hotmail.com>
2019-03-02 20:05:39 -0300

    Update menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'desafio2';
   userToken: any;
+  isShown: boolean = false;
 
   constructor() {
     this.userToken = localStorage.getItem('userToken');
